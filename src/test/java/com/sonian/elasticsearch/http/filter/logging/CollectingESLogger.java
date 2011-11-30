7ad5f591a062607d4204a2e256261e3c26a0ef6e
Igor Motov <igor@motovs.org>
2011-11-30 18:33:22 -0500

    [eng-955] fix race condition in logger test

--- src/test/java/com/sonian/elasticsearch/http/filter/logging/CollectingESLogger.java
+++ src/test/java/com/sonian/elasticsearch/http/filter/logging/CollectingESLogger.java
@@ -2,14 +2,16 @@ package com.sonian.elasticsearch.http.filter.logging;
 
 import org.elasticsearch.common.logging.support.AbstractESLogger;
 
-import java.io.PrintWriter;
-
 /**
  * @author imotov
  */
-public class PrintWriterESLogger extends AbstractESLogger {
+public class CollectingESLogger extends AbstractESLogger {
 
-    private PrintWriter out;
+    public interface LogMessageCollector {
+        public void log(String message);
+    }
+
+    private LogMessageCollector out;
 
     private Level level;
 
@@ -17,7 +19,7 @@ public class PrintWriterESLogger extends AbstractESLogger {
 
     public enum Level {TRACE, DEBUG, INFO, WARN, ERROR}
 
-    public PrintWriterESLogger(String level, PrintWriter out, String prefix, String name) {
+    public CollectingESLogger(String level, LogMessageCollector out, String prefix, String name) {
         super(prefix);
         this.out = out;
         this.name = name;
@@ -26,67 +28,52 @@ public class PrintWriterESLogger extends AbstractESLogger {
 
     @Override
     protected void internalTrace(String msg) {
-        out.println("TRACE:" + msg);
-        out.flush();
+        log("TRACE", msg);
     }
 
     @Override
     protected void internalTrace(String msg, Throwable cause) {
-        out.println("TRACE:" + msg);
-        cause.printStackTrace(out);
-        out.flush();
+        log("TRACE", msg, cause);
     }
 
     @Override
     protected void internalDebug(String msg) {
-        out.println("DEBUG:" + msg);
-        out.flush();
+        log("DEBUG", msg);
     }
 
     @Override
     protected void internalDebug(String msg, Throwable cause) {
-        out.println("DEBUG:" + msg);
-        cause.printStackTrace(out);
-        out.flush();
+        log("DEBUG", msg, cause);
     }
 
     @Override
     protected void internalInfo(String msg) {
-        out.println("INFO:" + msg);
-        out.flush();
+        log("INFO", msg);
     }
 
     @Override
     protected void internalInfo(String msg, Throwable cause) {
-        out.println("INFO:" + msg);
-        cause.printStackTrace(out);
-        out.flush();
+        log("INFO", msg, cause);
     }
 
     @Override
     protected void internalWarn(String msg) {
-        out.println("WARN:" + msg);
-        out.flush();
+        log("WARN", msg);
     }
 
     @Override
     protected void internalWarn(String msg, Throwable cause) {
-        out.println("WARN:" + msg);
-        cause.printStackTrace(out);
-        out.flush();
+        log("WARN", msg, cause);
     }
 
     @Override
     protected void internalError(String msg) {
-        out.println("ERROR:" + msg);
-        out.flush();
+        log("ERROR", msg);
     }
 
     @Override
     protected void internalError(String msg, Throwable cause) {
-        out.println("ERROR:" + msg);
-        cause.printStackTrace(out);
-        out.flush();
+        log("ERROR", msg, cause);
     }
 
     @Override
@@ -122,5 +109,13 @@ public class PrintWriterESLogger extends AbstractESLogger {
     @Override
     public boolean isErrorEnabled() {
         return level.ordinal() <= Level.ERROR.ordinal();
+    }
+
+    private void log(String level, String message, Throwable cause) {
+        out.log(level + ":" + message + "/" + cause);
+    }
+
+    private void log(String level, String message) {
+        out.log(level + ":" + message);
     }
 }
